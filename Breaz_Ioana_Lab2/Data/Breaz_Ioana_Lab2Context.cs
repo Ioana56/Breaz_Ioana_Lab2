5734a9fe1d378eda5f851994d0ef7439de5c3c9b
ioana56 <breaz@DESKTOP-QVV6HH5>
2022-11-13 20:58:20 +0200

    Add project files.

---
+++ Breaz_Ioana_Lab2/Data/Breaz_Ioana_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Breaz_Ioana_Lab2.Models;
+
+namespace Breaz_Ioana_Lab2.Data
+{
+    public class Breaz_Ioana_Lab2Context : DbContext
+    {
+        public Breaz_Ioana_Lab2Context (DbContextOptions<Breaz_Ioana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Breaz_Ioana_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
